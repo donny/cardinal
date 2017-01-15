3f051acb7c7bcae1dd1d18c3d0847fce9d64de70
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-01-16 09:24:07 +1100

    Add an initial version of BeautifulSoup4

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import logging
 from flask import Flask
+from bs4 import BeautifulSoup
+from google.appengine.api import urlfetch
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+
+    url = 'https://www.ozbargain.com.au/deals/feed'
+    try:
+        result = urlfetch.fetch(url, validate_certificate=True)
+        if result.status_code == 200:
+            data = result.content
+            soup = BeautifulSoup(data, 'xml')
+            items = soup.findAll('item')
+            for item in items:
+                title = item.title.text
+                link = item.link.text
+                description = item.description.text
+                print title.encode('utf-8')
+                print link.encode('utf-8')
+                print description.encode('utf-8')
+
+            return str(result.status_code)
+        else:
+            return str(result.status_code)
+    except urlfetch.Error:
+        logging.exception('Caught exception fetching url')
+        return 'ERROR'
 
 
 @app.errorhandler(500)
