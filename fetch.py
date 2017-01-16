25f8c49c968b0ef2e2faf8fece68314865a75bb0
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-01-16 12:28:34 +1100

    Add initial cron code

---
+++ fetch.py
@@ -0,0 +1,37 @@
+import logging
+import webapp2
+from bs4 import BeautifulSoup
+from google.appengine.api import urlfetch
+
+
+class Fetch(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+        url = 'https://www.ozbargain.com.au/deals/feed'
+        try:
+            result = urlfetch.fetch(url, validate_certificate=True)
+            if result.status_code == 200:
+                data = result.content
+                soup = BeautifulSoup(data, 'xml')
+                items = soup.findAll('item')
+                for item in items:
+                    title = item.title.text
+                    link = item.link.text
+                    description = item.description.text
+                    logging.info(title.encode('utf-8'))
+                    logging.info(description.encode('utf-8'))
+                    logging.info(link.encode('utf-8'))
+
+                self.response.write(str(result.status_code))
+            else:
+                self.response.write(str(result.status_code))
+        except urlfetch.Error:
+            logging.exception('Caught exception fetching url')
+            self.response.write("ERROR")
+
+app = webapp2.WSGIApplication([
+    ('/fetch', Fetch),
+], debug=True)
--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-import logging
 import webapp2
-from bs4 import BeautifulSoup
-from google.appengine.api import urlfetch
 
 
-class MainPage(webapp2.RequestHandler):
+class Main(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
-        url = 'https://www.ozbargain.com.au/deals/feed'
-        try:
-            result = urlfetch.fetch(url, validate_certificate=True)
-            if result.status_code == 200:
-                data = result.content
-                soup = BeautifulSoup(data, 'xml')
-                items = soup.findAll('item')
-                for item in items:
-                    title = item.title.text
-                    link = item.link.text
-                    description = item.description.text
-                    print title.encode('utf-8')
-                    print link.encode('utf-8')
-                    print description.encode('utf-8')
-
-                self.response.write(str(result.status_code))
-            else:
-                self.response.write(str(result.status_code))
-        except urlfetch.Error:
-            logging.exception('Caught exception fetching url')
-            self.response.write("ERROR")
-
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', Main),
 ], debug=True)
